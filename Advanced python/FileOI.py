44f14a0010d83d827950472aed66c3376b8f8f5d
Aksaule <aksaule124026@gmail.com>
2021-10-13 23:56:43 +0600

    Learn file io

---
+++ Advanced python/FileOI.py
@@ -0,0 +1,9 @@
+#Modes
+#r - opens a file for reading.
+#w - opens a file for writing
+#x - opens a file for exclusive creation. If the file already
+#exists, the operation fails
+#a - opens a file appending at the end 
+
+#Open file
+f = open("test.txt")
